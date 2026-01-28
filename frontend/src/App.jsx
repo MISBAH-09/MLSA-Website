9aa7fa1d525bdf84758ed7b3cb6c9b01547e9bbe
Misbah Sehar <misbahsahar09@gmail.com>
2026-01-28 21:12:02 +0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Tailwind v3 is WORKING 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
