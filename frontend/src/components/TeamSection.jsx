5306ec765a67bdeaac660504c8d3324ca257fa3a
Misbah Sehar <144267323+MISBAH-09@users.noreply.github.com>
2026-01-30 11:38:28 +0500

    Update TeamSection.jsx

--- frontend/src/components/TeamSection.jsx
+++ frontend/src/components/TeamSection.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
 import { Github, Linkedin, Twitter } from "lucide-react";
 import { Link } from "react-router-dom";
