6de488b0f07814c898ea99d59bad21914568af44
Urwa Kamal <fa23-bse-190@cuilahore.edu.pk>
2026-02-05 21:10:13 +0500

    Fix: updated favicon to MLSA logo

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -3,7 +3,7 @@ import StatsSection from "../components/StatsSection";
 import TeamSection from "../components/TeamSection";
 import WhyJoinSection from "../components/WhyJoinSection";
 import Footer from "../components/footer";
-import logo from '../images/mlsa.png';
+import logo from '../images/image.png';
 const Index = () => {
   // mlsaLogo = './vite.svg'
   return (
